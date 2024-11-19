4f70b4709944342df0c01aa7b4a5f1d81f3b0138
ikugox <aleksandarristic503@gmail.com>
2024-11-19 04:46:26 +0100

    file reading

--- main.go
+++ main.go
@@ -1,11 +1,65 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const folderPath = "./input"
+
 func main() {
-	arr := []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
-	freq := []int{5, 9, 12, 13, 16, 45, 2, 12}
+	entries, err := os.ReadDir(folderPath)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		if !strings.HasSuffix(entry.Name(), ".txt") {
+			continue
+		}
+		filePath := folderPath + "/" + entry.Name()
+		fmt.Printf("Found .txt file: %s\n", filePath)
 
-	root := generateHuffmanTree(arr, freq)
-	codes := generateHuffmanCodes(root)
+		data, freq := processFile(filePath)
+		root := generateHuffmanTree(data, freq)
+		codes := generateHuffmanCodes(root)
 
-	PrintHuffmanCodes(codes)
+		PrintHuffmanCodes(codes)
+	}
+}
+
+func processFile(filePath string) (data []rune, freq []int) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %v\n", filePath, err)
+		return nil, nil
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+
+	hashmap := make(map[rune]int)
+	for {
+		char, _, err := reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading file:", err)
+			return nil, nil
+		}
+		hashmap[char]++
+	}
+	var arr []rune
+	var occurances []int
+	for char, occurance := range hashmap {
+		arr = append(arr, char)
+		occurances = append(occurances, occurance)
+	}
+	return arr, occurances
 }
